c6784b8844876bf51cbbabd16130e0b85d8ab71b
leehyunbeom1227 <joar1227@naver.com>
2020-05-06 22:02:49 +0900

    Finished PaintJS

--- PaintJS/app.js
+++ PaintJS/app.js
@@ -3,12 +3,18 @@ const ctx =canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor")
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
+const INITIAL_COLOR = "#2c2c2c"
+const CANVAS_SIZE = 700;
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -37,6 +43,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event){
@@ -51,7 +58,26 @@ function handleModeClick(){
     } else {
         filling = true;
         mode.innerText ="Paint";
+        ctx.fillStyle = ctx.strokeStyle;
     }
+}
+
+function handleCanvasClick(){
+    if(filling){
+    ctx.fillRect(0,0, canvas.width, canvas.height)
+    }
+}
+
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[EXPORT]";
+    link.click();
 }
 
 if(canvas){
@@ -59,6 +85,8 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting)
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
+    canvas.addEventListener("click", handleCanvasClick)
+    canvas.addEventListener("contextmenu",handleCM)
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
@@ -69,6 +97,10 @@ if(range){
 
 if(mode){
     mode.addEventListener("click",handleModeClick);
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click",handleSaveClick);
 }
 
 function init(){
