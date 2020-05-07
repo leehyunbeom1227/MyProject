a0f6fda9661599137e3a580d32928b9fc31764e3
leehyunbeom1227 <joar1227@naver.com>
2020-05-07 23:56:13 +0900

    PaintJS revise and First Calculator

--- PaintJS/app.js
+++ PaintJS/app.js
@@ -5,14 +5,14 @@ const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
 
-const INITIAL_COLOR = "#2c2c2c"
+const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
 
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
 ctx.fillStyle = "white";
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -54,7 +54,7 @@ function handleRangeChange(event){
 function handleModeClick(){
     if(filling === true){
         filling = false;
-        mode.innerText ="Fill"
+        mode.innerText ="Fill";
     } else {
         filling = true;
         mode.innerText ="Paint";
@@ -64,7 +64,7 @@ function handleModeClick(){
 
 function handleCanvasClick(){
     if(filling){
-    ctx.fillRect(0,0, canvas.width, canvas.height)
+    ctx.fillRect(0,0, canvas.width, canvas.height);
     }
 }
 
@@ -81,15 +81,15 @@ function handleSaveClick(){
 }
 
 if(canvas){
-    canvas.addEventListener("mousemove", onMouseMove)
-    canvas.addEventListener("mousedown", startPainting)
-    canvas.addEventListener("mouseup", stopPainting)
-    canvas.addEventListener("mouseleave", stopPainting)
-    canvas.addEventListener("click", handleCanvasClick)
-    canvas.addEventListener("contextmenu",handleCM)
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu",handleCM);
 }
 
-Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 
 if(range){
     range.addEventListener("input", handleRangeChange);
@@ -102,9 +102,3 @@ if(mode){
 if(saveBtn){
     saveBtn.addEventListener("click",handleSaveClick);
 }
-
-function init(){
-
-}
-
-init();
