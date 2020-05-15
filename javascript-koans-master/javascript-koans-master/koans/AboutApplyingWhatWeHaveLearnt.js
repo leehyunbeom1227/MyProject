c43e96e4cd7155d3e71dc2b2382139d0230ce09b
leehyunbeom1227 <joar1227@naver.com>
2020-05-15 20:52:05 +0900

    Studying javascript by koans(Second but not _.)

--- javascript-koans-master/javascript-koans-master/koans/AboutApplyingWhatWeHaveLearnt.js
+++ javascript-koans-master/javascript-koans-master/koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +41,7 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(0);
   });
 
   /*********************************************************************************/
@@ -55,14 +55,17 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    var arr = _.range(1,1000);
+    var parse =_.filter(arr, number => number % 3 === 0 || number % 5 === 0 );
+    var sum = _.reduce(parse, function(number1, number2){ return number1 +number2;},0);
+    /* try chaining range() and reduce() */
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,15 +78,26 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
+    
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
+    var iDontGood_ = [];
 
+    for (i = 0; i < products.length; i+=1) {
+      for (j = 0; j < products[i].ingredients.length; j+=1) {
+          ingredientCount[products[i].ingredients[j]] = (ingredientCount[products[i].ingredients[j]] || 0) + 1;
+          iDontGood_.push(products[i].ingredients[j])
+      }
+  }
+  console.log(iDontGood_);
+  var findMushrooms = iDontGood_.indexOf("mushrooms"); // I can't good at _.
+  console.log(findMushrooms);
     /* chain() together map(), flatten() and reduce() */
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(findMushrooms);
   });
 
   /*********************************************************************************/
