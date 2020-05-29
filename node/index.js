64241161464414e7b86ca3d107b551fa409fe903
leehyunbeom1227 <joar1227@naver.com>
2020-05-29 21:48:53 +0900

    first Auth

--- node/index.js
+++ node/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser')
-const config = require('./config/key')
-
+const cookieParser = require('cookie-parser');
+const config = require('./config/key');
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 
@@ -27,7 +27,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.post('/register',(req,res) => {
+app.post('/api/users/register',(req,res) => {
 
     //회원 가입 할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터 베이스에 넣어준다.
@@ -42,7 +42,7 @@ app.post('/register',(req,res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email : req.body.email }, (err, user) => {
         if(!user) {
@@ -69,8 +69,22 @@ app.post('/login', (req, res) => {
             .status(200)
             .json({loginSuccess:true, userId: user._id})
         })    
-     })
-    
+     })    
+    })
+})
+
+
+app.get('/api/users/auth', auth , (req, res) =>{
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True
+    res.status(200).json({
+        _id : req.user._id,
+        isAdmin : req.user.role === 0 ? false :true,
+        isAuth:true,
+        email: req.user.email,
+        name : req.user.name,
+        lastname: req.user.lastname,
+        role : req.user.role,
+        image:req.user.image
     })
 })
 
